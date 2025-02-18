1589f745675e5d4282540434bc70fad75c944d79
Betty <btynyambura20@gmail.com>
2025-02-18 09:25:00 -0800

    mm

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,18 +4,21 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
+from os import getenv
 
 app = Flask(__name__)
 
 # Register the blueprint
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Close storage session"""
     storage.close()
 
+
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", "5000"))
+    HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    PORT = int(getenv("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,7 @@
 """View module for the API v1"""
 
 from flask import Blueprint
+from api.v1.views.index import *
 
 # Creating an instance of Blueprint for API v1 views
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-# Import everything from api.v1.views.index (this will import the routes defined there)
-from api.v1.views.index import *
