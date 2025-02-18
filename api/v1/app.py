6e25cb03235073618a25306073ba06c2b6f76da1
Betty <btynyambura20@gmail.com>
2025-02-18 09:39:17 -0800

    ,,

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,4 +21,4 @@ def teardown_db(exception):
 if __name__ == "__main__":
     HOST = getenv("HBNB_API_HOST", "0.0.0.0")
     PORT = int(getenv("HBNB_API_PORT", "5000"))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,9 @@
 """View module for the API v1"""
 
 from flask import Blueprint
-from api.v1.views.index import *
 
 # Creating an instance of Blueprint for API v1 views
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# Import views after defining app_views to avoid circular import
+from api.v1.views.index import *
