42cb8f1f25268a11e5b435bcdbe94dbd04227036
Betty <btynyambura20@gmail.com>
2025-02-19 02:21:41 -0800

    models to self

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -93,8 +93,8 @@ class FileStorage:
         if not cls:
             count = 0
             for clas in all_class:
-                count += len(models.storage.all(clas).values())
+                count += len(self.all(clas).values())
         else:
-            count = len(models.storage.all(cls).values())
+            count = len(self.all(cls).values())
 
         return count
